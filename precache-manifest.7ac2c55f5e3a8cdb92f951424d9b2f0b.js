3532342880732d04d933f09fb24bec6a14a03979
pratiklodha <pratiklodha01@gmail.com>
2019-08-13 12:40:18 +0530

    Updates

---
+++ precache-manifest.7ac2c55f5e3a8cdb92f951424d9b2f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf29443eac89f0f0e22e",
+    "url": "/roll-dice/static/js/runtime~main.cf29443e.js"
+  },
+  {
+    "revision": "e74134e9d2d517daccf3",
+    "url": "/roll-dice/static/js/main.e74134e9.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/roll-dice/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "e74134e9d2d517daccf3",
+    "url": "/roll-dice/static/css/main.69fc0b37.chunk.css"
+  },
+  {
+    "revision": "5ff2c91198cccc0f37b3230e26888a2b",
+    "url": "/roll-dice/index.html"
+  }
+];
